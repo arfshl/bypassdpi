b8bc4f1c0d4356f9b554ede310821f3d634a6304
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2025-04-22 12:56:57 +0700

    Add Script

---
+++ run_default.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+PUSHD "%~dp0"
+set _arch=x86
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
+IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
+PUSHD "%_arch%"
+
+start "" goodbyedpi.exe -f 2 -e 2  --reverse-frag --max-payload -p -r -s -m -w
+
+POPD
+POPD

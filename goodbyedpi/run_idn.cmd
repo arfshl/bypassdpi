cd6d8f1719a218d4bece1b8502a9e97b0b6816ad
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2025-04-23 15:05:43 +0700

    Update run_idn.cmd

--- goodbyedpi/run_idn.cmd
+++ goodbyedpi/run_idn.cmd
@@ -5,7 +5,7 @@ IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
 IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
 PUSHD "%_arch%"
 
-start "" goodbyedpi.exe -f 2 -e 2  --reverse-frag --max-payload -p -r -s -m -w
+start "" goodbyedpi.exe -p -f 2 -e 2 
 
 POPD
 POPD
